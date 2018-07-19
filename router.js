8a9f710f9c02d41f907b10cb21bb063dc7c213bb
MIClavelTe <miclavelte@gmail.com>
2018-07-18 18:56:45 -0700

    Get student info and show it

--- router.js
+++ router.js
@@ -1,10 +1,25 @@
+var profile = require('./profile.js');
+
 function userRoute(req, res) {
     var username = req.url.replace('/', "");
     if (username.length > 0) {
       res.setHeader('Content-Type', 'text/plain');
-      res.write('Header\n');
-      res.write(username + '\n');
-      res.end('End\n');
+
+      var student = new profile(username)
+      student.on('end', function(profileJSON) {
+        var values = {
+          avatarUrl: profileJSON.gravatar_url,
+          username: profileJSON.profile_name,
+          badges: profileJSON.badges.length,
+          points: profileJSON.points.total
+        }
+        res.write(values.username + '\n');
+        res.end(`${values.username} has ${values.badges} badge(s), ${values.points} points`);
+
+      });
+
+      // student.on('error', function(error) { 
+      // });
     }
 }
 
